490c824dc1bcaa5e91b4325f522dc4fbe892ed1e
zhouli@jike.com <zhouli@jike.com>
2013-08-29 14:15:55 +0800

    ADD:add spartutil ,modify filehelper

--- src/com/water/widget/File/FileHelper.java
+++ src/com/water/widget/File/FileHelper.java
@@ -11,13 +11,17 @@
 package com.water.widget.File;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
 
 /**
@@ -30,7 +34,32 @@ import org.apache.log4j.Logger;
 
 public final class FileHelper {
 
-  public static List<String> GetFileLines(String filePath) {
+  public static void copyFile(String oldPath, String newPath) {
+    try {
+      int bytesum = 0;
+      int byteread = 0;
+      File oldfile = new File(oldPath);
+      if (oldfile.exists()) { // 文件存在时
+        InputStream inStream = new FileInputStream(oldPath); // 读入原文件
+        FileOutputStream fs = new FileOutputStream(newPath);
+        byte[] buffer = new byte[4096];
+        int length;
+        while ((byteread = inStream.read(buffer)) != -1) {
+          bytesum += byteread; // 字节数 文件大小
+          System.out.println(bytesum);
+          fs.write(buffer, 0, byteread);
+        }
+        inStream.close();
+      }
+    } catch (Exception e) {
+      System.out.println("复制单个文件操作出错");
+      e.printStackTrace();
+
+    }
+
+  }
+
+  public static List<String> getFileLines(String filePath) {
     List<String> values = new ArrayList<String>();
 
     FileInputStream fInputStream = null;
@@ -57,6 +86,97 @@ public final class FileHelper {
     }
 
     return values;
+  }
+
+  public static void dirCopy(String src, String dst, List<String> excludes) {
+    // copy and delete
+    File dstFile = new File(dst);
+    File srcFile = new File(src);
+    if (!srcFile.exists()) {
+      LOG.error("Source Dir not exists:" + src);
+      return;
+    }
+    if (!dstFile.exists()) {
+      dstFile.mkdirs();
+    } else if (dstFile.isFile()) {
+      LOG.error("Path of dst file is a file:" + dst);
+      return;
+    }
+
+    // first copy
+    try {
+      FileUtils.copyDirectory(srcFile, dstFile);
+    } catch (IOException e) {
+      LOG.error("Copy Error:", e);
+    }
+    // delete excludes
+    deleteSubFiles(dstFile, excludes);
+  }
+
+  public static void deleteSubFiles(File dir, List<String> excludes) {
+    File[] files = dir.listFiles();
+    for (File file : files) {
+      if (file.isFile()) {
+        if (isExcludeFile(file.getName(), excludes)) {
+          file.delete();
+          System.out.println("Delete file:" + file.getAbsolutePath());
+        }
+      } else {
+        if (isExcludeFile(file.getName(), excludes)) {
+          deleteDirectory(file);
+          System.out.println("Delete dir:" + file.getAbsolutePath());
+        } else {
+          deleteSubFiles(file, excludes);
+        }
+      }
+    }
+  }
+
+  public static void deleteDirectory(File dirFile) {
+    if (!dirFile.exists() || !dirFile.isDirectory()) {
+      return;
+    }
+
+    File[] files = dirFile.listFiles();
+    for (int i = 0; i < files.length; i++) {
+      if (files[i].isFile()) {
+        files[i].delete();
+      } else {
+        deleteDirectory(files[i]);
+      }
+    }
+
+    dirFile.delete();
+  }
+
+  private static boolean isExcludeFile(String name, List<String> excludes) {
+    boolean include = false;
+    if (excludes != null) {
+      for (String filter : excludes) {
+        if (name.contains(filter)) {
+          include = true;
+          break;
+        }
+      }
+    }
+
+    return include;
+  }
+
+  public static void listDirFiles(File dirFile, List<String> subFiles) {
+    if (!dirFile.exists() || !dirFile.isDirectory()) {
+      return;
+    }
+
+    File[] files = dirFile.listFiles();
+    for (int i = 0; i < files.length; i++) {
+      if (files[i].isFile()) {
+        subFiles.add(files[i].getAbsolutePath());
+      } else {
+        listDirFiles(files[i], subFiles);
+      }
+    }
+
   }
 
   private static Logger LOG = Logger.getLogger(FileHelper.class);
---
+++ src/com/water/widget/File/SparkUtil.java
@@ -0,0 +1,7 @@
+package com.water.widget.File;
+
+public class SparkUtil {
+  public static String version() {
+    return "1.0.0";
+  }
+}
